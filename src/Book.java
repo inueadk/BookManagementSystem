29068f5ec52c5a4e70f5b498363bb8fd7afb594e
daeun <daeun@DESKTOP-LV88A00>
2020-04-12 01:12:51 +0900

    add,delete,view,edit book list by using an array

--- src/Book.java
+++ src/Book.java
@@ -4,26 +4,30 @@ public class Book {
 	String title;
 	String writer;
 	String publisher;
+	static int numBooksRegistered = 0;
 	
 	public Book(){
+		numBooksRegistered++;
 		
 	}
 	public Book(int id, String title){
 		this.id = id;
 		this.title = title;
+		numBooksRegistered++;
 	}
 	public Book(int id, String title, String writer, String publisher){
 		this.id = id;
 		this.title = title;
 		this.writer = writer;
 		this.publisher = publisher;
+		numBooksRegistered++;
 	}
 	
 	public void printInfo() {
-		System.out.println("Id : " + id);
-		System.out.println("Title : " + title);
-		System.out.println("Writer : " + writer);
-		System.out.println("Publisher : " + publisher);
+		System.out.println("Id : " + id + "  Title : " + title + "  Writer : " + writer + "  Publisher : " + publisher);
+//		System.out.println("Title : " + title);
+//		System.out.println("Writer : " + writer);
+//		System.out.println("Publisher : " + publisher);
 		
 	}
 	
--- src/BookManager.java
+++ src/BookManager.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BookManager {
-	Book book;
+	ArrayList<Book> books = new ArrayList<Book>();
 	Scanner input;
 	BookManager(Scanner input){
 		this.input = input;
 	}
 	
 	public void addBook() {
-		book = new Book();
+		Book book = new Book();
 		System.out.print("Book ID : ");
 		book.id = input.nextInt();
 		System.out.print("Book Title : ");
@@ -17,69 +18,87 @@ public class BookManager {
 		book.writer = input.next();
 		System.out.print("Publisher : ");
 		book.publisher = input.next();
+		books.add(book);
 	}
 	
 	public void deleteBook() {
 		System.out.print("Book ID : ");
 		int bookId = input.nextInt();
-		if(book==null) {
+		int index = -1;
+		for(int i=0; i<books.size();i++)
+		{
+			if(books.get(i).id == bookId) {
+				index = i;
+				break;
+			}
+		}
+		if(index >= 0)
+		{
+			books.remove(index);
+			Book.numBooksRegistered--;
+			System.out.println("The book "+bookId+"is deleted.");
+		}
+		else
+		{
 			System.out.println("The book has not been registered.");
 			return;
-		}
-		if(book.id == bookId) {
-			book = null;
-			System.out.println("The book is deleted.");
 		}
 	}
 	
 	public void editBook() {
 		System.out.print("Book ID : ");
 		int bookId = input.nextInt();
-		if(book.id == bookId) {
-			int num = -1;
-			while(num != 5) {
-				System.out.println("* Book Info Edit Menu *");
-				System.out.println("1. Edit ID");
-				System.out.println("2. Edit Title");
-				System.out.println("3. Edit Writer");
-				System.out.println("4. Edit Publisher");
-				System.out.println("5. Exit");
-				System.out.println("Select one number between 1 - 5  :)");
-				num = input.nextInt();
-				
-				if(num == 1) {
-					System.out.print("Book ID : ");
-					book.id = input.nextInt();
+		for(int i=0; i<books.size();i++)
+		{
+			Book book = books.get(i);
+			if(book.id == bookId) {
+				int num = -1;
+				while(num != 5) {
+					System.out.println("* Book Info Edit Menu *");
+					System.out.println("1. Edit ID");
+					System.out.println("2. Edit Title");
+					System.out.println("3. Edit Writer");
+					System.out.println("4. Edit Publisher");
+					System.out.println("5. Exit");
+					System.out.println("Select one number between 1 - 5  :)");
+					num = input.nextInt();
+
+					if(num == 1) {
+						System.out.print("Book ID : ");
+						book.id = input.nextInt();
+					}
+
+					else if(num == 2) {
+						System.out.print("Book Title : ");
+						book.title = input.next();
+					}
+
+					else if(num == 3) {
+						System.out.print("Writer : ");
+						book.writer = input.next();
+					}
+
+					else if(num == 4) {
+						System.out.print("Publisher : ");
+						book.publisher = input.next();
+					}
+
+					else {
+						continue;
+					}
 				}
-				
-				else if(num == 2) {
-					System.out.print("Book Title : ");
-					book.title = input.next();
-				}
-				
-				else if(num == 3) {
-					System.out.print("Writer : ");
-					book.writer = input.next();
-				}
-				
-				else if(num == 4) {
-					System.out.print("Publisher : ");
-					book.publisher = input.next();
-				}
-				
-				else {
-					continue;
-				}
-				
+				break;
 			}
 		}
 	}
 	
-	public void viewBook() {
-		System.out.print("Book ID : ");
-		int bookId = input.nextInt();
-		if(book.id == bookId) {
-			book.printInfo();
+	public void viewBooks() {
+//		System.out.print("Book ID : ");
+//		int bookId = input.nextInt();
+		System.out.println("num of registered books : "+Book.numBooksRegistered);
+		for(int i=0; i<books.size();i++)
+		{
+			books.get(i).printInfo();
 		}
 	}
 
